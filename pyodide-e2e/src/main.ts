55addcffa5242126acb316b3ff9efff699a47ebf
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-19 13:46:14 +0900

    Install and load the built wheel file into the test app

--- pyodide-e2e/src/main.ts
+++ pyodide-e2e/src/main.ts
@@ -1,7 +1,16 @@
+import wheelUrl from "../../dist/transformers_js_py-0.1.0-py3-none-any.whl?url";
+
 declare var loadPyodide: () => Promise<any>;
 
 async function main() {
-  let pyodide = await loadPyodide();
-  console.log(pyodide.runPython("1 + 2"));
+  const pyodide = await loadPyodide();
+
+  await pyodide.loadPackage("micropip");
+  const micropip = pyodide.pyimport("micropip");
+  await micropip.install(wheelUrl);
+
+  await pyodide.runPythonAsync(`
+import transformers
+`);
 }
 main();
