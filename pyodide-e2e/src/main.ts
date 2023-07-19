4c4fe6cdcea07b523979b446ef9e5196240ca30c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-19 13:36:34 +0900

    Import pyodide from CDN and run a simple script

--- pyodide-e2e/src/main.ts
+++ pyodide-e2e/src/main.ts
@@ -1,24 +1,7 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+declare var loadPyodide: () => Promise<any>;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function main() {
+  let pyodide = await loadPyodide();
+  console.log(pyodide.runPython("1 + 2"));
+}
+main();
