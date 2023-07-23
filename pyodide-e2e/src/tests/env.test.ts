ca875d01d54043fa25dc6a9c67b23fceb188eaea
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-07-23 09:07:36 +0900

    Remove a test

--- pyodide-e2e/src/tests/env.test.ts
+++ pyodide-e2e/src/tests/env.test.ts
@@ -15,18 +15,4 @@ describe("transformers.env", () => {
     const allowRemoteModels = await pyodide.runPythonAsync(`transformers.env.allowRemoteModels`);
     expect(allowRemoteModels).toBeTypeOf("boolean");
   });
-
-  it("is configurable", async () => {
-    await pyodide.runPythonAsync(`transformers.env.allowRemoteModels = False`);
-    const allowRemoteModels = await pyodide.runPythonAsync(`transformers.env.allowRemoteModels`);
-    expect(allowRemoteModels).toBe(false);
-
-    await pyodide.runPythonAsync(`transformers.env.allowRemoteModels = True`);
-    const allowRemoteModels2 = await pyodide.runPythonAsync(`transformers.env.allowRemoteModels`);
-    expect(allowRemoteModels2).toBe(true);
-
-    await pyodide.runPythonAsync(`transformers.env.allowRemoteModels = False`);
-    const allowRemoteModels3 = await pyodide.runPythonAsync(`transformers.env.allowRemoteModels`);
-    expect(allowRemoteModels3).toBe(false);
-  });
 })
