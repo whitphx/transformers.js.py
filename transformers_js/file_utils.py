64b2b331d24a81463fd11ffc3172fa3ba6363b31
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-10-19 22:29:10 +0900

    Add comments

--- transformers_js/file_utils.py
+++ transformers_js/file_utils.py
@@ -11,6 +11,16 @@ except ImportError:
 
 
 def as_url(file_path: str) -> str:
+    """For example, `pipeline('zero-shot-image-classification')`
+    requires a URL of the input image file in the browser environment.
+    This function converts a file path on Pyodide's virtual file system
+    to a URL that can be used as the input of such pipelines.
+
+    Internally, Transformers.js reads the input with this code:
+    https://github.com/xenova/transformers.js/blob/2.6.2/src/utils/image.js#L112-L113
+    Inside it `fetch()` is used. `fetch()` accepts a BlobURL.
+    """
+
     if js_Blob is None:
         raise RuntimeError("Blob is not available in this environment")
 
