9e0fa19b43460b8226c51f693d09a7be72ba24ca
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-10-25 13:06:11 +0900

    Add test for the automatic-speech-recognition model

--- pyodide-e2e/src/tests/pipeline.test.ts
+++ pyodide-e2e/src/tests/pipeline.test.ts
@@ -38,4 +38,32 @@ result = {item['label']: round(item['score'], 2) for item in data}
     const topLabel = Object.keys(resultObj).reduce((a, b) => resultObj[a] > resultObj[b] ? a : b);
     expect(topLabel).toEqual("lion");
   });
+
+  test("automatic-speech-recognition model", async () => {
+    await fetch("https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/jfk.wav")
+      .then((response) => response.blob())
+      .then((blob) => blob.arrayBuffer())
+      .then((arrayBuffer) => {
+        const fileData = new Uint8Array(arrayBuffer);
+        const filePath = "/tmp/jfk.wav";
+        pyodide.FS.writeFile(filePath, fileData);
+      });
+
+    await pyodide.runPythonAsync(`
+from transformers_js import import_transformers_js, as_url
+
+transformers = await import_transformers_js()
+pipeline = transformers.pipeline
+pipe = await pipeline('automatic-speech-recognition', 'Xenova/whisper-tiny.en')
+
+audio_file = "/tmp/jfk.wav"
+
+result = await pipe(as_url(audio_file))
+`);
+    const resultMap = await pyodide.globals.get("result").toJs();
+    const resultObj = Object.fromEntries(resultMap);
+    expect(resultObj).toEqual({
+      "text": " And so my fellow Americans ask not what your country can do for you, ask what you can do for your country."
+    });
+  });
 });
