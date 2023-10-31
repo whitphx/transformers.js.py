78dc03fdb8380dbaafb7108802e20b0b7525270c
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-10-31 14:38:55 +0900

    Create a `transformers_js` module as an alias

---
+++ transformers_js/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module was created as an alias for `transformers_js_py`
+due to a historical reason that the original package name was `transformers_js`.
+Ref: https://github.com/whitphx/transformers.js.py/issues/26
+"""
+
+from transformers_js_py import *  # noqa: F401, F403
