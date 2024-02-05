de60e64514d5390fd51877bb206d1ae00efe4771
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-02-05 17:36:14 +0900

    Create a special proxy class TjsTensorProxy dedicated for the Tensor class with the .numpy() method

---
+++ pyodide-e2e/src/tests/tensor.test.ts
@@ -0,0 +1,27 @@
+import type { PyodideInterface } from "pyodide";
+import { beforeEach, describe, it, expect } from "vitest";
+import { setupPyodideForTest } from "./utils";
+
+describe("Tensor", () => {
+  let pyodide: PyodideInterface;
+
+  beforeEach(async () => {
+    pyodide = await setupPyodideForTest(["numpy"]);
+  });
+
+  it("can be initialized and be converted into a numpy array", async () => {
+    await pyodide.runPythonAsync(`
+from transformers_js_py import import_transformers_js
+import numpy as np
+
+transformers = await import_transformers_js()
+
+Tensor = transformers.Tensor
+tensor = Tensor(np.array([1, 2, 3], dtype=np.float32))
+
+nparray = tensor.numpy()
+`);
+    const nparray = await pyodide.globals.get("nparray").toJs();
+    expect(nparray).toEqual(new Float32Array([1, 2, 3]));
+  })
+});
