cd78f1f743a8cdf15f9e21b122076ed67f2e1e60
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-04-27 02:47:29 +0100

    Add __getattr__ to transformers_js module (#144)

--- transformers_js/__init__.py
+++ transformers_js/__init__.py
@@ -5,3 +5,11 @@ Ref: https://github.com/whitphx/transformers.js.py/issues/26
 """
 
 from transformers_js_py import *  # noqa: F401, F403
+
+
+def __getattr__(name):
+    """Support using some objects proxying to Transformers.js API
+    without explicit call of `import_transformers_js` function."""
+    from transformers_js_py.lazy_import_proxies import get_deferred_import_proxy
+
+    return get_deferred_import_proxy(name)
