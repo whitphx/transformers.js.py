6e202de1d2b267433ed1283f340372533345d2e9
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-04-27 06:01:09 +0100

    Rename get_deferred_import_proxy to get_lazy_import_tjs_proxy (#146)

--- transformers_js/__init__.py
+++ transformers_js/__init__.py
@@ -10,6 +10,6 @@ from transformers_js_py import *  # noqa: F401, F403
 def __getattr__(name):
     """Support using some objects proxying to Transformers.js API
     without explicit call of `import_transformers_js` function."""
-    from transformers_js_py.lazy_import_proxies import get_deferred_import_proxy
+    from transformers_js_py.lazy_import_proxies import get_lazy_import_tjs_proxy
 
-    return get_deferred_import_proxy(name)
+    return get_lazy_import_tjs_proxy(name)
--- transformers_js_py/__init__.py
+++ transformers_js_py/__init__.py
@@ -8,6 +8,6 @@ __all__ = ["as_url", "read_audio", "import_transformers_js"]
 def __getattr__(name):
     """Support using some objects proxying to Transformers.js API
     without explicit call of `import_transformers_js` function."""
-    from .lazy_import_proxies import get_deferred_import_proxy
+    from .lazy_import_proxies import get_lazy_import_tjs_proxy
 
-    return get_deferred_import_proxy(name)
+    return get_lazy_import_tjs_proxy(name)
