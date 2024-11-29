121c26a849c7b99a31b34fa71091a1d199d9dab4
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-11-29 17:16:32 +0900

    Update tests

--- pyodide-e2e/src/tests/import.test.ts
+++ pyodide-e2e/src/tests/import.test.ts
@@ -1,6 +1,7 @@
 import type { PyodideInterface } from "pyodide";
 import { beforeEach, describe, it, expect } from "vitest";
 import { setupPyodideForTest } from "./utils";
+import { version } from "os";
 
 describe("import_transformers_js", () => {
   let pyodide: PyodideInterface;
@@ -9,46 +10,51 @@ describe("import_transformers_js", () => {
     pyodide = await setupPyodideForTest();
   });
 
-  it("can import specific version of Transformers.js", async () => {
-    await pyodide.runPythonAsync(
-      `from transformers_js_py import import_transformers_js`,
-    );
-
-    await pyodide.runPythonAsync(
-      `transformers = await import_transformers_js()`,
-    );
+  it("loads the latest version of Transformers.js by default", async () => {
     const expectedLatestVersion = await fetch(
       "https://registry.npmjs.org/@huggingface/transformers/latest",
     )
       .then((response) => response.json())
       .then((json) => json.version);
+
+    await pyodide.runPythonAsync(
+      `
+from transformers_js_py import import_transformers_js
+
+transformers = await import_transformers_js()
+`,
+    );
+
     expect(pyodide.runPython(`transformers.env.version`)).toBe(
       expectedLatestVersion,
     );
+  });
 
-    await pyodide.runPythonAsync(
-      `transformers = await import_transformers_js("2.4.2")`,
-    );
-    expect(pyodide.runPython(`transformers.env.version`)).toBe("2.4.2");
+  [
+    { versionOrUrl: "2.4.2", expectedVersion: "2.4.2" },
+    { versionOrUrl: "2.7.0", expectedVersion: "2.7.0" },
+    {
+      versionOrUrl: "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.1",
+      expectedVersion: "2.17.1",
+    },
+    { versionOrUrl: "3.0.0", expectedVersion: "3.0.0" },
+    {
+      versionOrUrl:
+        "https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.1.0",
+      expectedVersion: "3.1.0",
+    },
+  ].forEach(({ versionOrUrl, expectedVersion }) => {
+    it(`can import specific version of Transformers.js. (${versionOrUrl} => ${expectedVersion})`, async () => {
+      await pyodide.runPythonAsync(
+        `from transformers_js_py import import_transformers_js`,
+      );
 
-    await pyodide.runPythonAsync(
-      `transformers = await import_transformers_js("2.7.0")`,
-    );
-    expect(pyodide.runPython(`transformers.env.version`)).toBe("2.7.0");
-
-    await pyodide.runPythonAsync(
-      `transformers = await import_transformers_js("https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.1")`,
-    );
-    expect(pyodide.runPython(`transformers.env.version`)).toBe("2.17.1");
-
-    await pyodide.runPythonAsync(
-      `transformers = await import_transformers_js("3.0.0")`,
-    );
-    expect(pyodide.runPython(`transformers.env.version`)).toBe("3.0.0");
-
-    await pyodide.runPythonAsync(
-      `transformers = await import_transformers_js("https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.1.0")`,
-    );
-    expect(pyodide.runPython(`transformers.env.version`)).toBe("3.1.0");
+      await pyodide.runPythonAsync(
+        `transformers = await import_transformers_js("${versionOrUrl}")`,
+      );
+      expect(pyodide.runPython(`transformers.env.version`)).toBe(
+        expectedVersion,
+      );
+    });
   });
 });
