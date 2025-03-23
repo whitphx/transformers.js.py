8abfad39806d09133ebb1bbab3b794ebfc9a8de4
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-03-23 18:50:03 +0900

    Fix _version.py

--- src/transformers_js_py/_version.py
+++ src/transformers_js_py/_version.py
@@ -1,6 +1,8 @@
 import importlib.metadata
 
+__version__: str | None = None
+
 try:
     __version__ = importlib.metadata.version(__name__)
 except importlib.metadata.PackageNotFoundError:
-    __version__ = None
+    pass
